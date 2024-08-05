8ee864b95d33825c501a9c66225166a796899401
musxeto <mustafamalikawan786@gmail.com>
2024-08-05 14:52:55 +0500

    real

--- real/models.py
+++ real/models.py
@@ -5,3 +5,6 @@ class Contact(models.Model):
     email = models.EmailField()
     message = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
